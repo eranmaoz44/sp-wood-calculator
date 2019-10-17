04d5e44be825b8c64bb7bbfaa6f2463a90c7e646
Eran <e.magnex@gmail.com>
2019-10-17 20:53:51 +0300

    commited all changes

--- main.py
+++ main.py
@@ -13,8 +13,10 @@ def add_to_dict(dict, key, amount):
 
 
 if __name__ == '__main__':
-    beds = [('windows', 90, 190), ('full', 140, 190), ('full', 90, 200),
-            ('full', 140, 190), ('windows', 140, 190), ('full', 160, 200)]
+    # beds = [('windows', 90, 190), ('full', 140, 190), ('full', 90, 200),
+    #         ('full', 140, 190), ('windows', 140, 190), ('full', 160, 200)]
+    beds = [('windows', 80, 190), ('full', 140, 190), ('full', 140, 190), ('full', 160, 200)]
+    #beds = []
     counter = {}
     for bed in beds:
         add_to_dict(counter, '{0}/14.5/3.2'.format(bed[1] + 10), 4)
@@ -27,7 +29,7 @@ if __name__ == '__main__':
             if bed[1] >= 120:
                 add_to_dict(counter, '{0}/7/3.2'.format(round((bed[1] + 10 - 29) / 5.0)), 4)
             else:
-                add_to_dict(counter, '24/7/3.2', 2)
+                add_to_dict(counter, '{0}/7/3.2'.format(round((bed[1] + 10 - 29) / 3.0)), 2)
             add_to_dict(counter, '45/14.5/3.2', 2)
         add_to_dict(counter, '{0}/14.5/3.2'.format(bed[2]), 2)
         if bed[2] == 190:
@@ -48,10 +50,12 @@ if __name__ == '__main__':
         elif bed[2] == 200:
             add_to_dict(counter, '{0}/9.2/2'.format(bed[1]), 11)
 
-    bed_heads = [('C', 180), ('C', 180), ('full', 160), ('C', 160),
-                 ('full', 160), ('full', 140), ('D', 160),
-                 ('full', 180), ('C', 140), ('C', 140), ('C', 160), ('C', 160)]
+    # bed_heads = [('C', 180), ('C', 180), ('full', 160), ('C', 160),
+    #              ('full', 160), ('full', 140), ('D', 160),
+    #              ('full', 180), ('C', 140), ('C', 140), ('C', 160), ('C', 160)]
 
+    bed_heads = [('C', 100), ('C', 180), ('C', 140), ('C', 160)]
+    #bed_heads = []
     for bed_head in bed_heads:
         if bed_head[0] == 'full':
             add_to_dict(counter, '{0}/14.5/3.2'.format(bed_head[1] + 1), 2)
@@ -73,6 +77,19 @@ if __name__ == '__main__':
             add_to_dict(counter, '{0}/14.5/3.2'.format(bed_head[1] + 1), 2)
             add_to_dict(counter, '{0}/3.2/3.2'.format(bed_head[1] + 1), 2)
             add_to_dict(counter, '86.5/14.5/3.2', 2)
+
+    framed_jewish_beds = [('full', 160, 190)]
+
+    for framed_jewish_bed in framed_jewish_beds:
+        if framed_jewish_bed[0] == 'full':
+            add_to_dict(counter, '{0}/14.5/3.2'.format(framed_jewish_bed[1] + 10), 4)
+            add_to_dict(counter, '{0}/3.2/3.2'.format(framed_jewish_bed[1] + 10), 2)
+            add_to_dict(counter, '{0}/7/3.2'.format(framed_jewish_bed[1] + 10), 2)
+            add_to_dict(counter, '86.5/14.5/3.2', 3)
+            add_to_dict(counter, '45/14.5/3.2', 3)
+        add_to_dict(counter, '{0}/14.5/3.2'.format(framed_jewish_bed[2]), 1)
+        add_to_dict(counter, '{0}/14.5/3.2'.format(framed_jewish_bed[2]-1), 1)
+
 
     # print(str(sorted(counter.items(), key=lambda x: order_function(x[0]), reverse=True)).replace(',', ',\n'))
 

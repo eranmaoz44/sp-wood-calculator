7778f84287b6dfe40b7cc21027b81bd4b3fbd68e
Eran <e.magnex@gmail.com>
2020-01-29 14:39:00 +0200

    added existing woods and diff calculation

--- main.py
+++ main.py
@@ -1,9 +1,62 @@
 import collections
 
+existing_pieces = \
+    [('200/14.5/3.2', 16),
+     ('199/14.5/3.2', 3),
+     ('190/14.5/3.2', 13),
+     ('189/14.5/3.2', 1),
+     ('170/14.5/3.2', 0),
+     ('161/14.5/3.2', 2),
+     ('150/14.5/3.2', 10),
+     ('141/14.5/3.2', 0),
+     ('130/14.5/3.2', 6),
+     ('100/14.5/3.2', 8),
+     ('90/14.5/3.2', 8),
+     ('86.5/14.5/3.2', 7),
+     ('45/14.5/3.2', 19),
+     ('35/14.5/3.2', 10),
+     ('90/9.2/3.2', 20),
+     ('180/9.2/2', 28),
+     ('160/9.2/2', 0),
+     ('140/9.2/2', 33),
+     ('120/9.2/2', 22),
+     ('90/9.2/2', 26),
+     ('80/9.2/2', 22),
+     ('190/7/3.2', 6),
+     ('170/7/3.2', 7),
+     ('161/7/3.2', 4),
+     ('150/7/3.2', 5),
+     ('141/7/3.2', 0),
+     ('130/7/3.2', 3),
+     ('120/7/3.2', 5),
+     ('100/7/3.2', 3),
+     ('90/7/3.2', 0),
+     ('80/7/3.2', 3),
+     ('28/7/3.2', 21),
+     ('24/7/3.2', 29),
+     ('23/7/3.2', 0),
+     ('20/7/3.2', 36),
+     ('196/3.2/3.2', 18),
+     ('193/3.2/3.2', 13),
+     ('190/3.2/3.2', 8),
+     ('176/3.2/3.2', 4),
+     ('175/3.2/3.2', 16),
+     ('170/3.2/3.2', 5),
+     ('161/3.2/3.2', 6),
+     ('156/3.2/3.2', 36),
+     ('150/3.2/3.2', 7),
+     ('141/3.2/3.2', 1),
+     ('136/3.2/3.2', 6),
+     ('130/3.2/3.2', 8),
+     ('126/3.2/3.2', 1),
+     ('120/3.2/3.2', 3),
+     ('100/3.2/3.2', 4),
+     ('90/3.2/3.2', 2)]
+
 
 def order_function(key):
     split = key.split('/')
-    return float(split[1]) * 1000 + float(split[0])
+    return float(split[1]) * 10000 + float(split[2]) * 100 + float(split[0]) * 1
 
 
 def add_to_dict(dict, key, amount):
@@ -16,9 +69,9 @@ if __name__ == '__main__':
 
     # beds = [('full', 180, 200), ('full', 140, 190), ('windows', 140, 190), ('windows', 160, 190), ('full', 120, 190),
     #         ('full', 180, 200)]
-    beds = [('windows', 140, 190), ('windows', 140, 190), ('windows', 140, 190), ('windows', 140, 190),
-            ('windows', 140, 190), ('windows', 140, 190), ('windows', 120, 190), ('full', 180, 200),
-            ('windows', 90, 200), ('full', 160, 200), ('full', 120, 190), ('full', 80, 190), ('full', 90, 200)]
+    beds = [('windows', 160, 200), ('windows', 160, 190), ('full', 160, 190), ('full', 160, 200),
+            ('windows', 140, 190), ('full', 140, 190), ('full', 120, 190), ('full', 80, 190),
+            ('full', 90, 200), ('full', 180, 200)]
     # beds = []
     counter = {}
     for bed in beds:
@@ -53,11 +106,10 @@ if __name__ == '__main__':
         elif bed[2] == 200:
             add_to_dict(counter, '{0}/9.2/2'.format(bed[1]), 11)
 
+    # bed_heads = [('C', 160), ('C', 140), ('C', 180), ('C', 140)]
+    bed_heads = [('C', 120), ('C', 140), ('C', 160), ('C', 180), ('full', 140), ('full', 160), ('D', 140)]
 
-    #bed_heads = [('C', 160), ('C', 140), ('C', 180), ('C', 140)]
-    bed_heads = [('C', 160), ('C', 160), ('C', 140), ('C', 180)]
-
-    #bed_heads = []
+    # bed_heads = []
     for bed_head in bed_heads:
         if bed_head[0] == 'full':
             add_to_dict(counter, '{0}/14.5/3.2'.format(bed_head[1] + 1), 2)
@@ -80,11 +132,10 @@ if __name__ == '__main__':
             add_to_dict(counter, '{0}/3.2/3.2'.format(bed_head[1] + 1), 2)
             add_to_dict(counter, '86.5/14.5/3.2', 2)
 
+    # framed_jewish_beds = [('windows', 160, 200), ('full', 160, 190)]
 
-    #framed_jewish_beds = [('windows', 160, 200), ('full', 160, 190)]
-
-    framed_jewish_beds = [('windows', 180,200), ('windows', 160, 190), ('windows', 160, 200)]
-    #framed_jewish_beds = []
+    framed_jewish_beds = [('full', 180, 200), ('windows', 160, 190), ('full', 160, 190)]
+    # framed_jewish_beds = []
     for framed_jewish_bed in framed_jewish_beds:
         add_to_dict(counter, '{0}/14.5/3.2'.format(framed_jewish_bed[1] + 10), 4)
         add_to_dict(counter, '{0}/3.2/3.2'.format(framed_jewish_bed[1] + 10), 2)
@@ -99,7 +150,18 @@ if __name__ == '__main__':
 
     # print(str(sorted(counter.items(), key=lambda x: order_function(x[0]), reverse=True)).replace(',', ',\n'))
 
-    for wow in sorted(counter.items(), key=lambda x: order_function(x[0]), reverse=True):
+    counter_sorted = sorted(counter.items(), key=lambda x: order_function(x[0]), reverse=True)
+
+    for wow in counter_sorted:
+        y = wow[0].split('/')
+        print('אורן מוקצע פ.ע.               {0}              {1}               {2}'.format(y[1] + '/' + y[2], y[0],
+                                                                                            wow[1]))
+
+    print('now considering what we have')
+
+    counter_diff = [(key, value-dict(existing_pieces)[key]) for (key,value) in counter_sorted if value-dict(existing_pieces)[key] > 0]
+
+    for wow in counter_diff:
         y = wow[0].split('/')
         print('אורן מוקצע פ.ע.               {0}              {1}               {2}'.format(y[1] + '/' + y[2], y[0],
                                                                                             wow[1]))
